46b41501dc4bf080f1d4e3aae4ec7b747a0a4e40
ellasmith2090 <ellasmith200900@gmail.com>
2025-06-14 17:00:41 +0100

    Add Event model

---
+++ models/Event.js
@@ -0,0 +1,56 @@
+// models/Event.js
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+require("mongoose-type-email");
+const Utils = require("../Utils");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: mongoose.SchemaTypes.Email,
+      required: true,
+      unique: true,
+    },
+    accessLevel: {
+      type: Number,
+      required: true,
+      enum: [1, 2],
+      default: 1,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+    avatar: {
+      type: String,
+      default: "/images/default-avatar.png", // Static default image
+    },
+    isFirstLogin: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await Utils.hashPassword(this.password);
+  }
+  next();
+});
+
+module.exports = mongoose.model("User", userSchema);
